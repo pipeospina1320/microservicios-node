c8a65bbff1e002935f23a483676d30e0f1e702a7
Juan Felipe Tangarife <ospinafelipe17@gmail.com>
2022-08-31 12:41:19 -0500

    configuracion lint

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,8 @@ import config from '../config';
 const app = express();
 app.use(express.json());
 
-app.get('/', (_req, resp) => {
-    console.log('algo');
-    resp.send('pong prueba dev');
-});
 
 const PORT = config.api.port; 
 app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
